9e4203d61b2c3ebf5dcdf695aa78e376f88cf4b7
uduak <lorduduak@gmail.com>
2022-03-11 13:57:30 +0100

    added public directory

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!-- this page contains basic routing -->
+
+<?php 
+
+    require "../app/core/init.php";
+
+
+    // if no page name is set, use "home" as default value
+    $controller = $_GET['page_name'] ?? "home";
+
+    strtolower($controller); //convert value to lowercase
+
+    // if a file exists with controller name on controllers folder, require
+    if (file_exists("../app/controllers/" . $controller . ".php")) {
+        require "../app/controllers/" . $controller . ".php";
+    } else {
+        echo "controller not found";
+    }
